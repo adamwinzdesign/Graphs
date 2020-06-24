40c629b1dd155c59d62b56eb6d15daab5f44c87b
adamwinzdesign <adamwinzdesign@gmail.com>
2020-06-24 14:35:12 -0500

    finished adv.py

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -8,13 +8,25 @@ from ast import literal_eval
 # Load world
 world = World()
 
-
 # You may uncomment the smaller graphs for development and testing purposes.
 # map_file = "maps/test_line.txt"
 # map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
 map_file = "maps/main_maze.txt"
+
+class Queue():
+    def __init__(self):
+        self.queue = []
+    def enqueue(self, value):
+        self.queue.append(value)
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        else: 
+            return None
+    def size(self):
+        return len(self.queue)
 
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
@@ -25,11 +37,110 @@ world.print_rooms()
 
 player = Player(world.starting_room)
 
+# keep track of rooms visited
+visited = {}
+visited[player.current_room.id] = True
+
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
 traversal_path = []
 
+rooms_to_visit = []
 
+def traverse():
+    found_exit = True
+    while found_exit:
+        found_exit = False
+        # determine which exits are available for the current room
+        exits = player.current_room.get_exits()
+        current = player.current_room
+        possible_rooms = []
+        for direction in exits:
+            if current.get_room_in_direction(direction).id not in visited:
+                possible_rooms.append((current.get_room_in_direction(direction), direction))
+        # If we aren't currently at a dead end:
+        if len(possible_rooms) > 0:
+            room_to_traverse = possible_rooms[0]
+            for i in range(len(possible_rooms)):
+                if len(possible_rooms[i][0].get_exits()) < 2:
+                    room_to_traverse = possible_rooms[i]
+                    break
+                if possible_rooms[i][1] is 'w':
+                    room_to_traverse = possible_rooms[i]
+                    break
+                elif possible_rooms[i][1] is 's':
+                    room_to_traverse = possible_rooms[i]
+            # add additional rooms to rooms to visit cache
+            for room in possible_rooms:
+                if room != room_to_traverse:
+                    rooms_to_visit.append(room[0].id)
+            room, direction = room_to_traverse
+            # move to chosen room
+            player.travel(direction)
+            traversal_path.append(direction)
+            # add the chosen room to the visited cache
+            visited[room.id] = True
+            found_exit = True
+
+# Find shortest path to the unexplored room using BFS
+def find_shortest_path_to_unexplored(destination):
+    visited_room = set()
+
+    # Queue to store paths as they are built
+    path_as_built = Queue()
+    # Queue to store rooms as they are explored
+    rooms_explored = Queue()
+
+    path_as_built.enqueue([])
+    rooms_explored.enqueue(player.current_room)
+
+    while path_as_built.size() > 0:
+        path = path_as_built.dequeue()
+        current = rooms_explored.dequeue()
+        if current.id not in visited_room:
+            visited_room.add(current.id)
+            # once destination is reached, return the path
+            if current.id == destination:
+                return path
+            exits = current.get_exits()
+            for direction in exits:
+                path_copy = list(path)
+                path_copy.append(direction)
+                path_as_built.enqueue(path_copy)
+                rooms_explored.enqueue(current.get_room_in_direction(direction))
+    # if no path to destination is found
+    return None
+
+# traverse path passed into parameters
+def find_unexplored(path):
+    for direction in path:
+        player.travel(direction)
+        traversal_path.append(direction)
+    visited[player.current_room.id] = True
+
+# explore maze until all rooms are visited
+while len(world.rooms) > len(visited):
+    traverse()
+    # if at a dead end but there are still remaining rooms
+    if len(visited) != len(world.rooms):
+        # find closest unexplored room
+        paths = []
+        # find shortest path to each unexplored room
+        for unvisited in rooms_to_visit:
+            paths.append(find_shortest_path_to_unexplored(unvisited))
+        shortest_path = None
+        first_iter = True
+        for path in paths:
+            if first_iter:
+                shortest_path = path
+                first_iter = False
+                continue
+            if len(path) <= len(shortest_path):
+                shortest_path = path
+        # travel to closest unexplored room
+        find_unexplored(shortest_path)
+        # remove that room fro rooms to visit cache
+        rooms_to_visit.remove(player.current_room.id)
 
 # TRAVERSAL TEST
 visited_rooms = set()
@@ -51,12 +162,12 @@ else:
 #######
 # UNCOMMENT TO WALK AROUND
 #######
-player.current_room.print_room_description(player)
-while True:
-    cmds = input("-> ").lower().split(" ")
-    if cmds[0] in ["n", "s", "e", "w"]:
-        player.travel(cmds[0], True)
-    elif cmds[0] == "q":
-        break
-    else:
-        print("I did not understand that command.")
+# player.current_room.print_room_description(player)
+# while True:
+#     cmds = input("-> ").lower().split(" ")
+#     if cmds[0] in ["n", "s", "e", "w"]:
+#         player.travel(cmds[0], True)
+#     elif cmds[0] == "q":
+#         break
+#     else:
+#         print("I did not understand that command.")
--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -13,33 +13,91 @@ class Graph:
         """
         Add a vertex to the graph.
         """
-        pass  # TODO
+        if vertex_id in self.verticies:
+            raise Exception(f'A vertex with vertex id: {vertex_id} already exists!')
+        else:
+            self.verticies[vertex_id] = set()
 
     def add_edge(self, v1, v2):
         """
         Add a directed edge to the graph.
         """
-        pass  # TODO
+        if v1 in self.verticies and v2 in self.verticies:
+            self.verticies[v1].add(v2)
+        else:
+            raise IndexError('At least one of the given verticies does not exist in this graph!')
 
     def get_neighbors(self, vertex_id):
         """
         Get all neighbors (edges) of a vertex.
         """
-        pass  # TODO
+        return self.verticies[vertex_id]
 
     def bft(self, starting_vertex):
         """
         Print each vertex in breadth-first order
         beginning from starting_vertex.
         """
-        pass  # TODO
+        if starting_vertex not in self.verticies:
+            raise Exception('Could not locate starting vertex!')
+        visited = {}
+        verticies_to_visit = Queue()
+        current = starting_vertex
+
+        while current != None:
+            if current in visited:
+                if verticies_to_visit.size() is 0:
+                    current = None
+                    break
+                current = verticies_to_visit.dequeue()
+                continue
+            visited[current] = self.vertices[current]
+            neighbors = self.get_neighbors(current)
+            if len(neighbors) == 0:
+                if verticies_to_visit.size() == 0:
+                    current = None
+                    break
+                current = verticies_to_visit.dequeue()
+            else:
+                for vert in neighbors:
+                    if vert in visited:
+                        continue
+                    verticies_to_visit.dequeue(vert)
 
     def dft(self, starting_vertex):
         """
         Print each vertex in depth-first order
         beginning from starting_vertex.
         """
-        pass  # TODO
+        visited = {}
+        vertices_to_visit = Stack()
+        if starting_vertex not in self.verticies:
+            raise Exception(f'There is not a vertex with id: {starting_vertex} in the graph!')
+        else:
+            continue
+        current = starting_vertex
+        while current != None:
+            if current in visited:
+                if vertices_to_visit.size == 0:
+                    current = Nonebreak
+                current = vertices_to_visit.pop()
+                continue
+            visited[current] = self.vertices[current]
+            neighbors = self.get_neighbors(current)
+            if len(neighbors) == 0:
+                if vertices_to_visit.size() == 0:
+                    current = None
+                    break
+                current = vertices_to_visit.pop()
+            else:
+                for vert in neighbors:
+                    if vert in visited:
+                        continue
+                    vertices_to_visit.push(vert)
+                if vertices_to_visit.size() == 0:
+                    current = None
+                    break
+                current = vertices_to_visit.pop()
 
     def dft_recursive(self, starting_vertex):
         """
@@ -48,7 +106,20 @@ class Graph:
 
         This should be done using recursion.
         """
-        pass  # TODO
+       visited = {}
+       def traverse(vertex, values=[]):
+           if vertex not in visited:
+               visited[vertex] = True
+               values.append(vertex)
+            for vert in self.vertices[vertex]:
+                if v not in visited:
+                    visited[vert] = True
+                    values.append(vert)
+                    traverse(vert)
+            return values
+        order_list = traverse(starting_vertex)
+        for node in order_list:
+            print(node)
 
     def bfs(self, starting_vertex, destination_vertex):
         """
@@ -56,7 +127,37 @@ class Graph:
         starting_vertex to destination_vertex in
         breath-first order.
         """
-        pass  # TODO
+        if starting_vertex not in self.verticies:
+            raise Exception(f'There is no vertex with id: {starting_vertex} in the graph!')
+        if destination_vertex not in self.vertices:
+            raise Exception(f'There is no vertex with id: {destination_vertex} in the graph!')
+        visited = {}
+        distance = {}
+        previous = {}
+        vertices_to_visit = Queue()
+        visited[starting_vertex] = True
+        distance[starting_vertex] = 0
+        vertices_to_visit.enqueue(starting_vertex)
+
+        while vertices_to_visit.size() != 0:
+            current = vertices_to_visit.dequeue()
+            if len(self.vertices[current]) == 0:
+                continue
+            for vert in self.vertices[current]:
+                if v not in visited:
+                    visited[vert] = True
+                    distance[vert] = distance[current] + 1
+                    previous[vert] = current
+                    vertices_to_visit.enqueue(vert)
+                    if vert == destination_vertex:
+                        break
+        short_path = []
+        current = destination_vertex
+        short_path.append(current)
+        while current in prev:
+            short_path.insert(0, previous[current])
+            current = previous[current]
+        return short_path
 
     def dfs(self, starting_vertex, destination_vertex):
         """
